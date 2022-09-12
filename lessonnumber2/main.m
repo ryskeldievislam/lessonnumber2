0995854eaa6f2bfbda1026ba8c6f6a060c1c10bc
Arsen <turatbekuuluarsen@gmail.com>
2022-09-12 19:03:03 +0600

    Initial Commit

---
+++ lessonnumber2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  lessonnumber2
+//
+//  Created by Mac on 12/9/22.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
